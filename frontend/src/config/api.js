6e7eeb635b89d12c89c531cc79619305f6a4a452
NDOUR <adama.ndour@cimmyt.org>
2025-07-16 14:06:39 +0300

    adding .env.production and .env.development files

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL; //|| "http://localhost:3001";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 // Function to initialize RAG system
 export const initializeRAG = async () => {
